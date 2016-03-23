4f8de43a1b5d45a49b8438d77796ac01a4d21db3
DizzyDez <desc0137@algonquinlive.com>
2016-03-23 19:51:33 -0400

    retried after it didn't work

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $html = $('html');
 var $ball = $('.ball');
 var $picker = $('form');
-var $input = $('#colour-pick');
+var $input = $('#colour-picker');
 
 $picker.on('change', function () {
   console.log('works');
