9b4418a5dcddbfa4d7bed2adfb9d9a6abb71a075
DizzyDez <desc0137@algonquinlive.com>
2016-03-23 17:57:17 -0400

    fixed js, +form +input

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 var $html = $('html');
 var $ball = $('.ball');
+var $picker = $('form');
+var $input = $('#colour-pick');
+
+$picker.on('change', function () {
+  $ball.css('background-color', $input.val());
+});
